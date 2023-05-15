5104be0c3adf02ac7d986643a877f70aca473129
lambang <sasangkakurniawan.com>
2023-05-15 14:40:47 +0700

    UseReff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import PlaceContentCenter from './components/PlaceContentCenter.jsx';
-import Todo from './components/Todo.jsx';
+import Button from './components/button.jsx';
+import Card from './components/card.jsx';
+import Input from './components/input.jsx';
+import { useRef } from 'react';
+
 function App() {
+    const inputReff = useRef(null);
+    
+    function handleClick () {
+        // inputReff.current.focus();
+       inputReff.current.focus();
+    }
     return (
         <PlaceContentCenter>
-            <Todo />
+            <Card>
+                <Card.Title>UseRef Hooks</Card.Title>
+                <Card.Body>
+                    <div className='flex items-center gap-2'>
+                    <Input placeholder={"Email"}  isFouced className={"border border-slate-500"}/><br/>
+                    <Input placeholder={"Password"} isFocued className={"border border-slate-500"}/>
+                    <Button onClick={handleClick}>Login</Button>
+                    </div>
+                </Card.Body>
+                <Card.Footer>
+                    {/* Your click {ticker.current} times */}
+                </Card.Footer>
+            </Card>
         </PlaceContentCenter>
     );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,26 @@
-/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
-export default function Input({ type: string = 'text', ...props }) {
+/* eslint-disable no-undef */
+import { useRef } from 'react';
+import { useEffect } from 'react';
+
+// eslint-disable-next-line react/display-name
+const Input = ({ isFocused = false ,type='text',...props}) => {
+    const input = useRef(null);
+
+    useEffect(() => {
+        if (isFocused) {
+            input.current.focus();
+        }
+    }, []);
+
     return (
         <input
+            ref={input}
             {...props}
-            className={
-                'transition duration-300 w-full focus:outline-none focus:ring border-black focus:ring-blue-200 focus:border-blue-400 text-blackshadow-sm rounded-lg'
-            }
-            type='text'
+            className='transition duration-300 w-full focus:outline-none focus:ring border-black focus:ring-blue-200 focus:border-blue-400 text-blackshadow-sm rounded-lg'
+            type={type}
         />
     );
-}
+};
+
+export default Input;
